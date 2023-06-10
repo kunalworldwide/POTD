7d5fad5afbd21b99cfa3886e39c2dbbe9ce74512
Kunal Das <kunalworldwide@gmail.com>
2023-06-11 00:03:30 +0530

    Create 11-06-2023.py

---
+++ 11-06-2023.py
@@ -0,0 +1,7 @@
+class Solution:
+   def update(self, a, n, updates, k):
+       # Your code goes here
+       for i in range(0,k):
+           a[updates[i]-1] += 1
+       for i in range(1,n):
+           a[i] += a[i-1]
